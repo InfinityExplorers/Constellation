05c5eabd170718da9cc0c3b6afdaaec4ff4c39cc
StarL0st <65464466+StarL0st@users.noreply.github.com>
2023-05-14 00:11:36 -0700

    hope nothing breaks

---
+++ src/main/java/com/infinityexplorers/constellation/Constellation.java
@@ -0,0 +1,13 @@
+package com.infinityexplorers.constellation;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class Constellation {
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+}
